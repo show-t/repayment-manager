bda22da90fef6c2ddb905ad64a3f79fb19036c44
sakura <sakura@sakura-mm.local>
2021-10-23 01:47:33 +0900

    feat(request.vue): Validationの追加

--- pages/request.vue
+++ pages/request.vue
@@ -1,155 +1,165 @@
 <template>
     <v-app>
-        <v-card>
-            <v-card-title>立替金支払い請求フォーム</v-card-title>
-            <v-card-text>
-                <p>立替日</p>   
-                <v-menu>
-                    <template v-slot:activator={on}>
-                        <v-text-field
-                            v-on="on"
-                            readonly
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-card>
+                <v-card-title>立替金支払い請求フォーム</v-card-title>
+                <v-card-text>
+                    <p>立替日</p>   
+                    <v-menu>
+                        <template v-slot:activator={on}>
+                            <v-text-field
+                                v-on="on"
+                                readonly
+                                v-model="reqData.date"
+                                :rules="[rules.required]"
+                                label=""
+                                outlined
+                                required
+                            >
+                            </v-text-field>
+                        </template>
+                        <v-date-picker
                             v-model="reqData.date"
-                            label=""
-                            outlined
-                            required
+                            no-title
+                            scrollable
                         >
-                        </v-text-field>
-                    </template>
-                    <v-date-picker
-                        v-model="reqData.date"
-                        no-title
-                        scrollable
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                text
+                                color="primary"
+                            >
+                            Cancel
+                            </v-btn>
+                        </v-date-picker>
+                    </v-menu>
+                    
+                    
+                    <p>内容</p>
+                    <v-text-field
+                    v-model="reqData.contents"
+                    :rules="[rules.required]"
+                    label=""
+                    outlined
+                    required
                     >
-                        <v-spacer></v-spacer>
-                        <v-btn
-                            text
-                            color="primary"
-                        >
-                        Cancel
-                        </v-btn>
-                    </v-date-picker>
-                </v-menu>
-                
-                
-                <p>内容</p>
-                <v-text-field
-                v-model="reqData.contents"
-                label=""
-                outlined
-                required
-                >
-                </v-text-field>
-                <p>立替者</p>
-                <v-select
-                    v-model="reqData.repayer"
-                    :items="users"
+                    </v-text-field>
+                    <p>立替者</p>
+                    <v-select
+                        v-model="reqData.repayer"
+                        :items="users"
+                        :rules="[rules.required]"
+                        label=""
+                        outlined
+                    ></v-select>
+                    <p>対象者</p>
+                    <v-select
+                        v-model="reqData.targets"
+                        :items="users"
+                        :rules="[rules.required,rules.counter]"
+                        chips
+                        label=""
+                        multiple
+                        outlined
+                    ></v-select>
+                    <p>金額 (※単価を記入)</p>
+                    <v-text-field
+                    type="number"
+                    v-model="reqData.amount"
+                    :rules="[rules.required]"
                     label=""
+                    prefix="¥"
                     outlined
-                ></v-select>
-                <p>対象者</p>
-                <v-select
-                    v-model="reqData.targets"
-                    :items="users"
-                    chips
-                    label=""
-                    multiple
-                    outlined
-                ></v-select>
-                <p>金額 (※単価を記入)</p>
-                <v-text-field
-                v-model="reqData.amount"
-                label=""
-                outlined
-                required
-                >
-                </v-text-field>
-                <p>備考</p>
-                <v-textarea
-                    v-model="reqData.remark"    
-                    name="input-7-4"
-                    label=""
-                    outlined
-                ></v-textarea>
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions>
-                <v-dialog v-model="dialogConfirm">
-                    <template v-slot:activator="{on,attrs}">
-                        <v-btn
-                            v-bind="attrs"
-                            v-on="on"
-                            block
-                            x-large
-                        >
-                            送信する
-                        </v-btn>
-                    </template>
+                    required
+                    >
+                    </v-text-field>
+                    <p>備考</p>
+                    <v-textarea
+                        v-model="reqData.remark"    
+                        name="input-7-4"
+                        label=""
+                        outlined
+                    ></v-textarea>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions>
+                    <v-dialog v-model="dialogConfirm">
+                        <template v-slot:activator="{on,attrs}">
+                            <v-btn
+                                :disabled="!valid"
+                                v-bind="attrs"
+                                v-on="on"
+                                block
+                                x-large
+                            >
+                                送信する
+                            </v-btn>
+                        </template>
+                        <v-card>
+                            <v-card-title class="text-h5 grey lighten-2">
+                                送信確認
+                            </v-card-title>
+                            <v-card-text>
+                                <v-container>
+                                    <v-row><v-col>以下の内容で送信します</v-col></v-row>
+                                </v-container>
+                                <v-container>
+                                    <v-row><v-col>【立替日】</v-col><v-col>{{reqData.date}}</v-col></v-row>
+                                    <v-row><v-col>【内　容】</v-col><v-col>{{reqData.contents}}</v-col></v-row>
+                                    <v-row><v-col>【立替者】</v-col><v-col>{{reqData.repayer}}</v-col></v-row>
+                                    <v-row><v-col>【対象者】</v-col><v-col>{{reqData.targets}}</v-col></v-row>
+                                    <v-row><v-col>【金　額】</v-col><v-col>¥{{reqData.amount}}</v-col></v-row>
+                                    <v-row><v-col>【備　考】</v-col><v-col>{{reqData.remark}}</v-col></v-row>
+                                </v-container>
+                            </v-card-text>
+                            <v-divider></v-divider>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                    color="primary"
+                                    text
+                                    @click="submit"
+                                >
+                                    OK
+                                </v-btn>
+                                <v-btn
+                                    text
+                                    color="primary"
+                                    @click="dialogConfirm = false"
+                                >
+                                    Cancel
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-card-actions>
+                <v-dialog v-model="dialogSuccess">
                     <v-card>
-                        <v-card-title class="text-h5 grey lighten-2">
-                            送信確認
-                        </v-card-title>
-                        <v-card-text>
-                            <v-container>
-                                <v-row><v-col>以下の内容で送信します</v-col></v-row>
-                            </v-container>
-                            <v-container>
-                                <v-row><v-col>【立替日】</v-col><v-col>{{reqData.date}}</v-col></v-row>
-                                <v-row><v-col>【内　容】</v-col><v-col>{{reqData.contents}}</v-col></v-row>
-                                <v-row><v-col>【立替者】</v-col><v-col>{{reqData.repayer}}</v-col></v-row>
-                                <v-row><v-col>【対象者】</v-col><v-col>{{reqData.targets}}</v-col></v-row>
-                                <v-row><v-col>【金　額】</v-col><v-col>{{reqData.amount}}</v-col></v-row>
-                                <v-row><v-col>【備　考】</v-col><v-col>{{reqData.remark}}</v-col></v-row>
-                            </v-container>
-                        </v-card-text>
-                        <v-divider></v-divider>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn
-                                color="primary"
-                                text
-                                @click="submit"
-                            >
-                                OK
-                            </v-btn>
-                            <v-btn
-                                text
-                                color="primary"
-                                @click="dialogConfirm = false"
-                            >
-                                Cancel
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
+                            <v-card-title class="text-h5 grey lighten-2">
+                                登録完了
+                            </v-card-title>
+                            <v-card-text>
+                                <v-container>
+                                    <v-row><v-col>{{resData.ids.length}}件登録しました</v-col></v-row>
+                                </v-container>
+                                <v-container>
+                                    <v-row v-for="id in resData.ids" :key="id"><v-col>{{id}}</v-col></v-row>
+                                </v-container>
+                            </v-card-text>
+                            <v-divider></v-divider>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                    text
+                                    color="primary"
+                                    @click="dialogSuccess = false"
+                                >
+                                    OK
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
                 </v-dialog>
-            </v-card-actions>
-            <v-dialog v-model="dialogSuccess">
-                <v-card>
-                        <v-card-title class="text-h5 grey lighten-2">
-                            登録完了
-                        </v-card-title>
-                        <v-card-text>
-                            <v-container>
-                                <v-row><v-col>{{resData.ids.length}}件登録しました</v-col></v-row>
-                            </v-container>
-                            <v-container>
-                                <v-row v-for="id in resData.ids" :key="id"><v-col>{{id}}</v-col></v-row>
-                            </v-container>
-                        </v-card-text>
-                        <v-divider></v-divider>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn
-                                text
-                                color="primary"
-                                @click="dialogSuccess = false"
-                            >
-                                OK
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-            </v-dialog>
-        </v-card>
+            </v-card>
+        </v-form>
     </v-app>
 </template>
 <script lang="ts">
@@ -166,6 +176,10 @@ export default Vue.extend({
                 amount:"",
                 remark:""
             },
+            rules:{
+                required:(val:any)=> !!val || '必須項目',
+                counter:(val:any)=>val.length > 0 ||'1人以上選択'
+            },
             resData:{
                 ids:[],
             },
@@ -173,13 +187,15 @@ export default Vue.extend({
             endpoint: "https://script.google.com/macros/s/AKfycbzLSPfDaJBd85eoTZpptb3ceUvp34VQj_Y7nC7vNurczRiX7J3TR9AgabNQHoUIJKSr/exec",
             dialogConfirm: false,
             dialogSuccess: false,
+            valid: false,
         }
     },
     methods:{
         submit:function(){
-            this.post()
-            this.dialogConfirm = false
-            
+            if((this.$refs.form as any).validate()){
+                this.post()
+                this.dialogConfirm = false
+            }
         },
         post:function(){
             var body:string = JSON.stringify(this.reqData)
@@ -196,22 +212,15 @@ export default Vue.extend({
                 })
         },
         clearReqData(){
-            this.reqData = {
-                date:"",
-                contents:"",
-                repayer:"",
-                targets:[],
-                amount:"",
-                remark:""
-            }
+            (this.$refs.form as any).reset()
         }
     },
-    mounted(){
-        const params = {
-            crossDomain: true
-        }
+    created(){
         axios.get(this.endpoint)
             .then(res=>this.users = res.data.users)
+    },
+    mounted(){
+        (this.$refs.form as any).validate()  
     }
 })
 </script>
