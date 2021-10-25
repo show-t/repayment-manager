439cb796e215a93a86f6c4a5389e4d8fc1548df1
sakura <sakura@sakura-mm.local>
2021-10-26 02:13:38 +0900

    feat: liff対応
    ユーザープロフィール表示テスト

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,25 @@
       <v-btn @click="movePage(item.to)" block x-large>{{ item.name }}</v-btn>
       <br />
     </div>
-    <p>{{loggedIn}}</p>
+    <p>{{ loggedIn }}</p>
+    <div v-if="loggedIn" class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img :src="pictureUrl" />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{ displayName }}</p>
+            <p class="subtitle is-6">@{{ userId }}</p>
+          </div>
+        </div>
+        <div class="content">
+          {{ statusMessage }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,20 +36,41 @@ export default Vue.extend({
         { name: "立替金支払い完了報告", to: "/report" },
         { name: "精算リストをみる", to: "/view" },
       ],
-      loggedIn:false,
+      loggedIn: false,
+      userId: "",
+      displayName: "",
+      pictureUrl: "https://bulma.io/images/placeholders/128x128.png",
+      statusMessage: "",
     };
   },
   methods: {
     movePage: function (path: string) {
       this.$router.push(path);
     },
+    getProfile(){
+      liff.getProfile()
+        .then(profile=>{
+          this.userId = profile.userId
+          this.displayName = profile.displayName
+          this.pictureUrl = profile.pictureUrl
+          this.statusMessage = profile.statusMessage
+        })
+        .catch(function(error) {
+          //alert('Error getting profile: ' + error)
+        })
+    }
   },
-  created(){
-    liff.init({
-      liffId: this.$config.LIFF_ID
-    }).then(()=>{
-      this.loggedIn = liff.isLoggedIn()
-    })
+  created() {
+    liff
+      .init({
+        liffId: this.$config.LIFF_ID,
+      })
+      .then(() => {
+        this.loggedIn = liff.isLoggedIn();
+      });
+  },
+  mounted(){
+    this.getProfile()
   }
 });
 </script>
